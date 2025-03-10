f026e0b7f10bedb056f2af88dd35da3152582c72
Rahaf <Rahafotaibe24@gmail.com>
2025-03-10 14:48:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
-from typing import Optional
-
 from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import numpy as np
 
 app = FastAPI()
 
+model = joblib.load('knn_model.joblib')
+scaler = joblib.load('scaler.joblib')
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+class InputFeatures(BaseModel):
+    minutes_played: int
+    appearance: int
+    highest_value: int
+
+
+def preprocessing(input_features: InputFeatures):
+    dict_f = {
+        'minutes_played': input_features.minutes_played,
+        'appearance': input_features.appearance,
+        'highest_value': input_features.highest_value
+    }
+    return dict_f
+
+
+@app.post("/predict")
+def predict(data: InputFeatures):
+    input_data = preprocessing(data)
+    input_array = np.array([list(input_data.values())]).reshape(1, -1)
+
+    # Transform input data using the scaler
+    input_scaled = scaler.transform(input_array)
+
+    # Make a prediction
+    prediction = model.predict(input_scaled)
+
+    # Return the prediction as a string instead of converting to int
+    return {"prediction": prediction[0]}  # No `int()` conversion needed
